eefb7378889a6183a949e68154a4bd75ceed8111
MinkiPoint-Sando52 <davids.sandovalb@konralorenz.edu.co>
2024-04-22 09:00:32 -0500

    Entrega Taller 2 - Juan Camilo Prieto Mestizo
    Entrega de los dos códigos, de la clase libro y la clase principal con las especificaciones de la guía. Documentado con la complejidad temporal.

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,5 +1,49 @@
 package com.mycompany.libropoo;
 
 public class Libro {
-    
-}
+
+    private String titulo;//O(1)
+    private String autor;//O(1)
+    private int anio;//O(1)
+    private String genero;//O(1)
+    private boolean leido;//O(1)
+
+    //Metodo Constructor
+    //Complejidad temporal: O(1) Tiempo constante.
+    public Libro(String titulo, String autor, int anio, String genero, boolean leido) {
+        this.titulo = titulo;//O(1)
+        this.autor = autor;//O(1)
+        this.anio = anio;//O(1)
+        this.genero = genero;//O(1)
+        this.leido = false;//O(1)
+    }//Cierre constructor
+
+    //Metodo para mostrar la informacion del libro
+    //Complejidad temporal: O(1) Tiempo constante.
+    public String mostrarInformacion() {
+        return "Titulo: " + titulo + ", Autor: " + autor + ", Año de publicación: " + anio + ", Género: " + genero;//O(1)
+    }//Cierre mostrarInformacion
+
+    //Metodo para validar el libro
+    //Complejidad temporal: O(1) Tiempo constante.
+    public void marcarLeido() {
+        leido = true;//O(1)
+
+    }//Cierre marcarLeido
+
+    //Metodo para retornar el dato de marcarLeido
+    //Complejidad temporal: O(1) Tiempo constante.
+    public boolean isLeido() {
+        return leido;//O(1)
+    }//Cierre isLeido
+
+    //Metodo para validar si es un libro antiguo
+    //Complejidad temporal: O(1) Tiempo constante.
+    public boolean esAntiguo() {
+        if (anio < 1974) {
+            return true;
+        }//O(1)
+        return false;//O(1)
+    }//Cierre esAntiguo
+
+}//Cierre class Libro
--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,5 +1,37 @@
 package com.mycompany.libropoo;
 
+import java.util.Scanner;
+
 public class Principal {
-    
-}
+
+    //Metodo para solicitar variables, crear objetos y ejecutar metodos
+    //Complejidad temporal: O(1) Tiempo constante.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Buen dia.");//O(1)
+        System.out.println("Por favor ingrese el titulo del libro:");//O(1)
+        String titulo = sc.nextLine();//O(1)
+        System.out.println("Ingrese el autor del libro:");//O(1)
+        String autor = sc.nextLine();//O(1)
+        System.out.println("Ingrese el género literario del libro:");//O(1)
+        String genero = sc.nextLine();//O(1)
+        System.out.println("Ingrese el año de publicación del libro");//O(1)
+        int anio = sc.nextInt();//O(1)
+
+        boolean leido = false;//O(1)
+
+        Libro libroUsuario = new Libro(titulo, autor, anio, genero, leido);//O(1)
+
+        libroUsuario.marcarLeido();
+        System.out.println(libroUsuario.mostrarInformacion());//O(1)
+        libroUsuario.isLeido();
+        if (libroUsuario.isLeido() == true) {
+            System.out.println("El libro " + titulo + " ya fue leído.");
+        } else {
+            System.out.println("No has leido " + titulo);
+        }//O(1)
+        if (libroUsuario.esAntiguo() == true) {
+            System.out.println("El libro es antiguo.");
+        }//Cierre if para determinar antiguedad//O(1)
+    }//Cierre main
+}//Cierre Class Principal
